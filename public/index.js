eec11d329fd33e200fe6b55c83311fd3870a6e86
Bardini <derekbardini@gmail.com>
2021-02-24 18:32:44 -0600

    file stucture modified

---
+++ public/index.js
@@ -0,0 +1,13 @@
+init();
+console.log("this one", location.search.split("=")[1]);
+async function init() {
+  if (location.search.split("=")[1] === undefined) {
+    const workout = await API.getLastWorkout();
+    if (workout) {
+      location.search = "?id=" + workout._id;
+    } else {
+      document.querySelector("#continue-btn").classList.add("d-none")
+    }
+  }
+}
+
---
+++ routes/apiroutes.js
@@ -0,0 +1,54 @@
+var db = require("../models");
+
+module.exports = function(app) {
+
+//   display workouts
+  app.get("/api/workouts", function(req, res) {
+    db.Workout.find({})
+      .then(function(results) {
+        console.log("RESULTS: ",results);
+        res.json(results);
+      })
+      .catch(function(err){
+        res.json(err);
+      });
+  });
+// add exercises to existing workout
+  app.put("/api/workouts/:id", function(req, res) {
+    console.log(req.params.id, " ballsacks");
+    const id = req.params.id
+    // console.log(location.search.split("=")[1], "AAAAAAAAAAAAAAAAAAAAaaa");
+    // console.log(res, "!!!!!!!!!!!!!!!!!!1111!"); ,id: req.params.id
+db.Workout.updateOne({ _id:  id }, { $push: { exercises: req.body } }, { new: true })
+    .then(function(results){
+      res.json(results);
+    })
+    .catch(function(err){
+      res.json(err)
+    })
+  });
+// add execsises to new workout
+  app.post("/api/workouts", function(req, res) {
+    console.log("post /api/workouts route hit!!!!!!!!1");
+   db.Workout.create(req.body)
+   .then(function(results){
+     res.json(results);
+   })
+   .catch(function(err){
+     res.json(err);
+   })
+   
+  });
+// get workouts within range
+  app.get("/api/workouts/range", function(req, res) {
+    db.Workout.find({}).sort({day:-1}).limit(7)
+    .then(function(results){
+      res.json(results);
+    })
+    .catch(function(err) {
+      res.json(err)
+    })
+  });
+
+
+};
